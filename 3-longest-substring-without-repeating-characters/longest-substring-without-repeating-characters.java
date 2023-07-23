a9a3b6445bdb433a528c4acb6bf286602884877e
achyutagupta <107514366+achyutagupta@users.noreply.github.com>
2023-07-23 14:56:15 +0530

    Time: 5 ms (92.06%) | Memory: 44 MB (36.10%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,24 +1,46 @@
+// class Solution {
+//     public int lengthOfLongestSubstring(String s) {
+//         int length = 0;
+//         int maxlength =0;
+//         int n = s.length();
+//         String str ="";
+//         for(int i=0;i<n;i++){
+//           char ch = s.charAt(i);
+//           CharSequence seq = new StringBuilder(1).append(ch);
+//           if(str.contains(seq)){
+//             int index = str.indexOf(ch);
+//             length = str.length()-index;
+//             str=str.substring(index+1)+ch;
+//             System.out.println(str+" "+length);
+//           }
+//           else{
+//             length++;
+//             maxlength = Math.max(maxlength , length);
+//             str = str + ch;
+//           }
+//         }
+//         return maxlength;
+//     }
+// }
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int length = 0;
-        int maxlength =0;
         int n = s.length();
-        String str ="";
-        for(int i=0;i<n;i++){
-          char ch = s.charAt(i);
-          CharSequence seq = new StringBuilder(1).append(ch);
-          if(str.contains(seq)){
-            int index = str.indexOf(ch);
-            length = str.length()-index;
-            str=str.substring(index+1)+ch;
-            System.out.println(str+" "+length);
-          }
-          else{
-            length++;
-            maxlength = Math.max(maxlength , length);
-            str = str + ch;
-          }
+        int maxLength = 0;
+        int startIndex = 0;
+
+        HashMap<Character, Integer> charMap = new HashMap<>();
+
+        for (int endIndex = 0; endIndex < n; endIndex++) {
+            char ch = s.charAt(endIndex);
+
+            if (charMap.containsKey(ch)) {
+                startIndex = Math.max(startIndex, charMap.get(ch) + 1);
+            }
+
+            charMap.put(ch, endIndex);
+            maxLength = Math.max(maxLength, endIndex - startIndex + 1);
         }
-        return maxlength;
+
+        return maxLength;
     }
 }
