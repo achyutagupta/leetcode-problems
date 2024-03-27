03b513ed3c31b4ca4eececed6589580e6a7f729c
Achyuta Gupta <107514366+achyutagupta@users.noreply.github.com>
2024-03-27 18:59:16 +0530

    Time: 2 ms (99.91%) | Memory: 55.3 MB (82.07%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -1,13 +1,37 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashSet<Integer> num = new HashSet<>();
-        for(int i=0;i<nums.length;i++){
-          if(num.contains(nums[i])){
-            return true;
-          }else{
-            num.add(nums[i]);
-          }
+        // HashMap<Integer,Integer> visited = new HashMap<>();
+        // for(int num : nums){
+        //     if(visited.containsKey(num) && visited.get(num)>=1)
+        //        return true;
+        //     visited.put(num,visited.getOrDefault(num,0)+1);
+        // }
+        // return false;
+        final int length = nums.length;
+        if (length == 1)
+            return false;
+        if (length == 2)
+            return nums[0] == nums[1];
+
+        for (int i = 1; i < length; i++) {
+            final int current = nums[i];
+            int j = i - 1, jval = nums[j];
+            if (current < jval) {
+                while (true) {
+                    nums[j + 1] = jval;
+                    if (j-- == 0)
+                        break;
+                    jval = nums[j];
+                    if (current >= jval)
+                        break;
+                }
+                nums[j + 1] = current;
+            }
+
+            if (j > -1 && current == jval)
+                return true;
         }
+
         return false;
     }
 }
